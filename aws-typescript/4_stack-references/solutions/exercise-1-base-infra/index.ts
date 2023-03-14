d215bcfaa710ea3c0016799d628369140f67d5d7
MitchellGerdisch <mitch@pulumi.com>
2023-03-13 21:39:35 -0500

    clean up solutions

--- aws-typescript/4_stack-references/solutions/exercise-1-base-infra/index.ts
+++ aws-typescript/4_stack-references/solutions/exercise-1-base-infra/index.ts
@@ -6,5 +6,7 @@ const config = new pulumi.Config();
 const baseName = config.get("baseName") || `${pulumi.getOrganization()}-${pulumi.getStack()}`.toLowerCase();
 
 // Use Pulumi-provided AWSX package for easy VPC and related resource creation.
-const vpc = new awsx.ec2.Vpc(`${baseName}-net`)
+const vpc = new awsx.ec2.Vpc(`${baseName}-net`);
 
+export const vpcId = vpc.vpcId;
+export const webserverSubnetId = vpc.publicSubnetIds[0]
